3183995602b0557f54c768dfa4f0e47045454086
João Daniel <jotaf.daniel@gmail.com>
2024-04-08 12:59:41 +0200

    fix Component to alternate between server and listener

--- src/main/java/it/unibz/butterfly_net/dashboard_server/Component.java
+++ src/main/java/it/unibz/butterfly_net/dashboard_server/Component.java
@@ -1,29 +1,42 @@
 package it.unibz.butterfly_net.dashboard_server;
 
-import it.unibz.butterfly_net.dashboard_server.application.DatabaseConnection;
-import it.unibz.butterfly_net.dashboard_server.application.InsertionListener;
-import it.unibz.butterfly_net.dashboard_server.application.PostgresqlRawDataRepository;
-import it.unibz.butterfly_net.dashboard_server.application.PostgresqlSeleniumRecordRepository;
+import it.unibz.butterfly_net.dashboard_server.application.*;
+import it.unibz.butterfly_net.dashboard_server.core.Dashboard;
 import it.unibz.butterfly_net.dashboard_server.core.RawDataProcessor;
+import it.unibz.butterfly_net.dashboard_server.core.repositories.ProjectTypeRepository;
+import it.unibz.butterfly_net.dashboard_server.core.repositories.RawDataRepository;
+import it.unibz.butterfly_net.dashboard_server.core.repositories.SeleniumRecordRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.Objects;
 
 public class Component {
     private static final Logger logger = LoggerFactory.getLogger(Component.class);
+    private static final String TYPE_LISTENER = "listener";
 
-    public static void main(String[] args) throws SQLException {
-        logger.info("Running...");
 
-        RawDataProcessor processor = new RawDataProcessor(
-                new PostgresqlRawDataRepository(),
-                new PostgresqlSeleniumRecordRepository()
-        );
+    public static void main(String[] args) {
+        String executionType = args[0];
 
-        Connection connection = DatabaseConnection.getConnection();
-        InsertionListener listener = new InsertionListener(connection, processor);
-        listener.run();
+        SeleniumRecordRepository seleniumRecordRepository = new PostgresqlSeleniumRecordRepository();
+        ProjectTypeRepository projectTypeRepository = new PostgresqlProjectTypeRepository();
+        RawDataRepository rawDataRepository = new PostgresqlRawDataRepository();
+
+        boolean isRunningListener = Objects.equals(executionType, TYPE_LISTENER);
+        if (isRunningListener) runListener(seleniumRecordRepository, rawDataRepository);
+        else runServer(seleniumRecordRepository, projectTypeRepository);
+    }
+
+    public static void runServer(SeleniumRecordRepository seleniumRecordRepository, ProjectTypeRepository projectTypeRepository) {
+        logger.info("Running listener");
+        Dashboard dashboard = new Dashboard(seleniumRecordRepository, projectTypeRepository);
+        new HttpServer(dashboard).run();
+    }
+
+    public static void runListener(SeleniumRecordRepository seleniumRecordRepository, RawDataRepository rawDataRepository) {
+        logger.info("Running server");
+        RawDataProcessor processor = new RawDataProcessor(rawDataRepository, seleniumRecordRepository);
+        new InsertionListener(processor).run();
     }
 }
